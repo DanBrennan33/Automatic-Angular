c10285888d0371e8f38bfb4cc11c71b36551ced0
DanBrennan33 <DanBrennan33@hotmail.com>
2018-01-14 13:11:12 -0500

    Angular routing with services, hero and details components.

---
+++ angular-heroes-of-chrono-trigger/src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Crono', desc: ''},
+  { id: 11, name: 'Marle', desc: ''},
+  { id: 12, name: 'Marle', desc: ''},
+  { id: 13, name: 'Lucca', desc: ''},
+  { id: 14, name: 'Frog', desc: ''},
+  { id: 15, name: 'Robo', desc: ''},
+  { id: 16, name: 'Ayla', desc: ''},
+  { id: 17, name: 'Magus', desc: ''},
+  { id: 18, name: 'Lavos', desc: ''},
+  { id: 19, name: 'Gurus', desc: ''},
+  { id: 20, name: 'Schala', desc: ''},
+  { id: 21, name: 'Dalton', desc: ''},
+  { id: 22, name: 'Zeal', desc: ''}
+];
