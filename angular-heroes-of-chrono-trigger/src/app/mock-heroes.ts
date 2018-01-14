ddb366771dd4eda43c2d260f4f84183c6572a014
DanBrennan33 <DanBrennan33@hotmail.com>
2018-01-14 13:42:44 -0500

    Routing complete.

--- angular-heroes-of-chrono-trigger/src/app/mock-heroes.ts
+++ angular-heroes-of-chrono-trigger/src/app/mock-heroes.ts
@@ -3,7 +3,6 @@ import { Hero } from './hero';
 export const HEROES: Hero[] = [
   { id: 10, name: 'Crono', desc: ''},
   { id: 11, name: 'Marle', desc: ''},
-  { id: 12, name: 'Marle', desc: ''},
   { id: 13, name: 'Lucca', desc: ''},
   { id: 14, name: 'Frog', desc: ''},
   { id: 15, name: 'Robo', desc: ''},
